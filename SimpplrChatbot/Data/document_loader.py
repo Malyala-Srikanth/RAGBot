6747e30cc42322a9f2d2e641ca13cb6cb246c411
Srikanth Malyala <malyala2srikanth@gmail.com>
2024-10-15 23:56:06 +0530

    Adding poetry for dependency manager and decoupling embedding based and keyword based retriver

--- SimpplrChatbot/Data/document_loader.py
+++ SimpplrChatbot/Data/document_loader.py
@@ -1,50 +1,24 @@
 import re
 import asyncio
 from typing import List
-from langchain.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter  # noqa: E501
-from langchain_experimental.text_splitter import SemanticChunker
+from langchain.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.docstore.document import Document
-from langchain_core.embeddings import Embeddings
-
-from langchain.vectorstores import ElasticVectorSearch
 from Utils.utils import logging
-from tenacity import retry, stop_after_attempt, wait_exponential
 
 
 class DocumentLoader:
-    def __init__(
-        self,
-        data_path: str,
-        index_name: str,
-        elasticsearch_url: str,
-        embeddings: Embeddings,
-    ):  # noqa: E501
+    def __init__(self, data_path: str):
         self.data_path = data_path
-        self.index_name = index_name
-        self.elasticsearch_url = elasticsearch_url
-        self.embeddings = embeddings
 
-    async def load_documents(self) -> List[str]:
-        # Load text files
-        text_loader = DirectoryLoader(
-            self.data_path, glob="**/*.txt", loader_cls=TextLoader
-        )
-        text_documents = await asyncio.to_thread(text_loader.load)
-
+    async def load_documents(self) -> List[Document]:
         # Load PDF files
         pdf_loader = DirectoryLoader(
             self.data_path, glob="**/*.pdf", loader_cls=PyPDFLoader
         )
         pdf_documents = await asyncio.to_thread(pdf_loader.load)
+        return pdf_documents
 
-        # Combine text and PDF documents
-        documents = text_documents + pdf_documents
-        return documents
-
-    async def restructure_documents(
-        self, documents: List[Document]
-    ) -> List[str]:  # noqa: E501
+    async def restructure_documents(self, documents: List[Document]) -> List[Document]:
         restructured_documents = []
         for doc in documents:
             page_content = doc.page_content
@@ -56,98 +30,21 @@ class DocumentLoader:
             page_content = re.sub(r"(\w{2,}\.)", r"\1\n", page_content)
 
             restructured_documents.append(
-                doc.__class__(page_content=page_content, metadata=metadata)
+                Document(page_content=page_content, metadata=metadata)
             )
         return restructured_documents
 
-    async def split_documents(
-        self,
-        documents: List[Document],
-        chunk_size: int = 1000,
-        chunk_overlap: int = 200,  # noqa: E501
-        type_of_splitter: str = "recursive",
-    ) -> List[Document]:
-        if type_of_splitter == "recursive":
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=chunk_size, chunk_overlap=chunk_overlap
-            )
-        elif type_of_splitter == "semantic":
-            text_splitter = SemanticChunker(
-                chunk_size=chunk_size, chunk_overlap=chunk_overlap
-            )
-        split_docs = await asyncio.to_thread(
-            text_splitter.split_documents, documents
-        )  # noqa: E501
-        return split_docs
-
-    @retry(
-        stop=stop_after_attempt(3),
-        wait=wait_exponential(multiplier=1, min=4, max=10),  # noqa: E501
-    )
-    async def generate_embeddings(self, texts):
-        try:
-            embeddings = await asyncio.to_thread(
-                self.embeddings.embed_documents, texts
-            )  # noqa: E501
-            if not embeddings:
-                raise ValueError("Embeddings list is empty")
-            return embeddings
-        except Exception as e:
-            logging.error(f"Error generating embeddings: {str(e)}")
-            raise
-
-    async def index_documents(
-        self, documents: List[Document]
-    ) -> ElasticVectorSearch:  # noqa: E501
-        logging.info(f"Indexing {len(documents)} documents")
-
-        if not documents:
-            raise ValueError("No documents to index")
-
-        texts = [doc.page_content for doc in documents]
-
-        try:
-            embeddings = await self.generate_embeddings(texts)
-            logging.info(f"Generated {len(embeddings)} embeddings")
-
-            vector_store = await asyncio.to_thread(
-                ElasticVectorSearch.from_documents,
-                documents,
-                self.embeddings,
-                elasticsearch_url=self.elasticsearch_url,
-                index_name=self.index_name,
-            )
-            return vector_store
-        except Exception as e:
-            logging.error(f"Error during indexing: {str(e)}")
-            raise
-
-    async def load_split_and_index(
-        self,
-        chunk_size: int = 1000,
-        chunk_overlap: int = 200,
-        type_of_splitter: str = "recursive",
-    ) -> ElasticVectorSearch:
-        logging.info("Loading documents")
+    async def load_and_process(self) -> List[Document]:
+        logging.info("Loading PDF documents")
         documents = await self.load_documents()
-        logging.info(f"Loaded {len(documents)} documents")
+        logging.info(f"Loaded {len(documents)} PDF documents")
         logging.info("Restructuring documents")
         restructured_docs = await self.restructure_documents(documents)
-        logging.info("Splitting documents")
-        split_docs = await self.split_documents(
-            restructured_docs, chunk_size, chunk_overlap, type_of_splitter
-        )
-        logging.info("Indexing documents")
-        vector_store = await self.index_documents(split_docs)
-        return vector_store
+        return restructured_docs
 
 
 if __name__ == "__main__":
-    from langchain.embeddings import OpenAIEmbeddings
-
-    embeddings = OpenAIEmbeddings()
     data_path = "/Users/malyala/Desktop/SimpplrChatbot/SimpplrChatbot/pdfs"
-    document_loader = DocumentLoader(
-        data_path, "simpplr-chatbot", "http://localhost:9200", embeddings
-    )  # noqa: E501
-    vector_store = asyncio.run(document_loader.load_split_and_index())
+    document_loader = DocumentLoader(data_path)
+    processed_docs = asyncio.run(document_loader.load_and_process())
+    print(f"Processed {len(processed_docs)} documents")
--- SimpplrChatbot/Data/query_helper.py
+++ SimpplrChatbot/Data/query_helper.py
@@ -1,12 +1,12 @@
-from langchain_core.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import ElasticVectorSearch
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-from langchain.retrievers import ElasticSearchBM25Retriever
 
 from Data.document_loader import DocumentLoader
+from Data.embedding_based_retriever import EmbeddingBasedRetriever
+from Data.keyword_match_retriever import KeywordMatchRetriever
 from API.settings import settings
 from Utils.utils import logger
+import asyncio
 
 
 class QueryHelper:
@@ -14,69 +14,46 @@ class QueryHelper:
         self.index_name = settings.INDEX_NAME
         self.approach = settings.RETRIEVER_APPROACH
         self.elasticsearch_url = settings.ELASTICSEARCH_URL
-        self.embeddings = OpenAIEmbeddings(model=settings.EMBEDDING_MODEL)
-        self.vector_store = ElasticVectorSearch(
-            index_name=self.index_name,
-            embedding_function=self.embeddings,
-            elasticsearch_url=self.elasticsearch_url,
-        )
-        self.document_loader = DocumentLoader(
-            data_path=settings.DATA_PATH,
-            index_name=self.index_name,
-            elasticsearch_url=self.elasticsearch_url,
-            embeddings=self.embeddings,
-        )
-        self.es_client = self.vector_store.client
+        self.es_client = None  # We'll initialize this only if needed for BM25
+        self.vector_store = None
+        self.qa_chain = None
+        # run intialize here
+        asyncio.run(self.initialize())
 
-    def query(self, query: str):
+    async def initialize(self):
+        self.document_loader = DocumentLoader(settings.DATA_PATH)
+        documents = await self.document_loader.load_and_process()
+
+        llm = ChatOpenAI(
+            temperature=settings.LLM_TEMPERATURE,
+            top_p=settings.LLM_TOP_P,
+            model_name=settings.LLM_MODEL,
+        )
+
         if self.approach == "embedding-based":
-            logger.info("Using embedding based retriever")
-            vector_store = self.document_loader.load_split_and_index(
-                chunk_size=settings.CHUNK_SIZE,
-                chunk_overlap=settings.CHUNK_OVERLAP,
-                type_of_splitter=settings.SPLITTER_TYPE,
+            self.embedding_based_retriever = EmbeddingBasedRetriever(
+                self.index_name, self.elasticsearch_url
             )
-
-            # Initialize language model
-            llm = ChatOpenAI(
-                temperature=settings.LLM_TEMPERATURE,
-                top_p=settings.LLM_TOP_P,
-                model_name=settings.LLM_MODEL,
-            )
-
-            # Initialize qa_chain
-            qa_chain = RetrievalQA.from_chain_type(
-                llm=llm,
-                chain_type="stuff",
-                retriever=vector_store.as_retriever(),
-                return_source_documents=True,
-            )
-
-            return qa_chain.invoke({"query": query})
+            await self.embedding_based_retriever.index_documents(documents)
+            self.vector_store = await self.embedding_based_retriever.get_vector_store()
+            retriever = self.vector_store.as_retriever()
         elif self.approach == "keyword-match":
-            logger.info("Using Keyword based retriever")
-            # Initialize BM25 retriever
-            bm25_retriever = ElasticSearchBM25Retriever(
-                client=self.es_client,
-                index_name=self.index_name,
-                k=settings.BM25_K_DOCUMENTS,
+            self.keyword_match_retriever = KeywordMatchRetriever(
+                self.index_name, self.elasticsearch_url
             )
+            await self.keyword_match_retriever.index_documents(documents)
+            retriever = self.keyword_match_retriever.get_retriever()
 
-            # Initialize language model
-            llm = ChatOpenAI(
-                temperature=settings.LLM_TEMPERATURE,
-                top_p=settings.LLM_TOP_P,
-                model_name=settings.LLM_MODEL,
-            )
-
-            # Initialize qa_chain with BM25 retriever
-            qa_chain = RetrievalQA.from_chain_type(
-                llm=llm,
-                chain_type="stuff",
-                retriever=bm25_retriever,
-                return_source_documents=True,
-            )
-
-            return qa_chain.invoke({"query": query})
         else:
-            raise NotImplementedError
+            raise NotImplementedError(f"Unsupported approach: {self.approach}")
+
+        self.qa_chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=retriever,
+            return_source_documents=True,
+        )
+
+    async def query(self, query: str):
+        logger.info(f"Using {self.approach} retriever")
+        return await self.qa_chain.ainvoke({"query": query})
